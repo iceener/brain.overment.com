e0fee2ee3bbae631b956d1b722545374970d9292
Adam Gospodarczyk <adam@overment.com>
2022-11-07 03:18:01 +0100

    feat(settings): Added favicons

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,17 @@ export default {
     base: '/',
     title: 'brain.overment.com',
     description: 'Everything I know',
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/img/favicons/mstile-150x150.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/img/favicons/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/img/favicons/favicon-16x16.png"}],
+        ['link', { rel: "manifest", href: "/assets/img/favicons/site.webmanifest"}],
+        ['link', { rel: "mask-icon", href: "/assets/img/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+        ['link', { rel: "shortcut icon", href: "/assets/img/favicons/favicon.ico"}],
+        ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+        ['meta', { name: "msapplication-config", content: "/assets/img/favicons/browserconfig.xml"}],
+        ['meta', { name: "theme-color", content: "#ffffff"}],
+    ],
     themeConfig: {
         logo: '/assets/img/logo-symbol.svg',
         sidebar: [
